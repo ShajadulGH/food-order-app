9642c99eee9c19879f3c1a67def281b90e1ba157
Shajadul <shajadulrahe@gmail.com>
2022-12-01 09:12:46 +0600

    Feat: Add CartButton

--- src/Components/Layout/Nav.js
+++ src/Components/Layout/Nav.js
@@ -1,12 +1,13 @@
 import Image from "./Image";
 import { Fragment } from "react";
 import styles from "./Nav.module.css";
+import CardButton from "./CartButton";
 const Nav = () => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h2>House of Burger</h2>
-        <button>Cart</button>
+        <CardButton></CardButton>
       </header>
       <Image></Image>
     </Fragment>
