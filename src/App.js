71dbbbb6f669087e4b3f8bc1711e79477cef264f
Shajadul <shajadulrahe@gmail.com>
2022-12-02 08:16:51 +0600

    Fix: Change Modal render position

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Nav from "./Components/Layout/Nav";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import Foods from "./Components/Foods/Foods";
+import Cart from "./Components/Cart/Cart";
+
 function App() {
+  const [modalShowHide, setModalShowHide] = useState(false);
+  const modalShow = () => {
+    setModalShowHide(true);
+  };
+  const modalHide = () => {
+    setModalShowHide(false);
+  };
   return (
     <Fragment>
-      <Nav />
+      {modalShowHide && <Cart onClick={modalHide} />}
+      <Nav onClick={modalShow} />
       <Foods />
     </Fragment>
   );
--- src/Components/Layout/Nav.js
+++ src/Components/Layout/Nav.js
@@ -1,24 +1,15 @@
 import Image from "./Image";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import styles from "./Nav.module.css";
 import CardButton from "./CartButton";
-import Cart from "../Cart/Cart";
-const Nav = () => {
-  const [modalShowHide, setModalShowHide] = useState(false);
-  const modalShow = () => {
-    setModalShowHide(true);
-  };
-  const modalHide = () => {
-    setModalShowHide(false);
-  };
+const Nav = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h2>House of Burger</h2>
-        <CardButton onClick={modalShow}></CardButton>
+        <CardButton onClick={props.onClick}></CardButton>
       </header>
       <Image></Image>
-      {modalShowHide && <Cart onClick={modalHide} />}
     </Fragment>
   );
 };
