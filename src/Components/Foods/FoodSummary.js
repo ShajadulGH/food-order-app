7b30402c5503043e8736a2364877569423a431cc
Shajadul <shajadulrahe@gmail.com>
2022-12-07 14:16:08 +0600

    fix: responsive summurry

--- src/Components/Foods/FoodSummary.js
+++ src/Components/Foods/FoodSummary.js
@@ -3,10 +3,10 @@ const FoodSummary = () => {
   return (
     <section className={styles.summary}>
       <h3>We Are Ready To Serve You</h3>
-      <p>
+      {/* <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
-      </p>
+      </p> */}
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
